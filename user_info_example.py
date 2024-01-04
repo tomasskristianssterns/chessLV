965690fb24dd95aacc6ffb87d482bf0e34eb6b08
tomasskristianssterns <tomass-kristians.sterns@edu.rtu.lv>
2024-01-04 23:35:05 +0200

    add keywords

--- user_info_example.py
+++ user_info_example.py
@@ -1,2 +1,14 @@
-PATH = r"C:\\Users\\{path}\\regulations" # add path to current workspace for tournament regulations
-FIDE_ID = None                           # change to your fide ID number
+PATH = r"C:\\Users\\{path}\\chessLV\\regulations" # add path to current workspace for tournament regulations
+FIDE_ID = ""                                      # change to your fide ID number
+MY_EMAIL = ""                                     # change to your email
+PASSWORD_KEY = ""                                 # change to your generated password key
+MESSAGE = '''                                     
+Labdien!
+
+Šis ir teksts.
+
+Ar cieņu
+Vārds Uzvārds
+
+Šī ziņa tika nosūtīta automātiski.
+                '''                               # change to message
