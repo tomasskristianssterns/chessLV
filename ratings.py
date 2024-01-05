06b508beef4fab26bf80373eb87f4a183d71c4d5
tomasskristianssterns <tomass-kristians.sterns@edu.rtu.lv>
2024-01-05 20:38:11 +0200

    get rating update

--- ratings.py
+++ ratings.py
@@ -4,18 +4,30 @@ from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 from user_info import FIDE_ID
 
-URL = f"https://ratings.fide.com/profile/{FIDE_ID}"
+def rating(event_type):
+    URL = f"https://ratings.fide.com/profile/{FIDE_ID}"
 
-ratings = {
-    "gray" : "Classic",
-    "red"  : "Rapid",
-    "blue" : "Blitz"
-}
+    ratings = {
+        "gray" : "Classic",
+        "red"  : "Rapid",
+        "blue" : "Blitz"
+    }
 
-response = requests.get(URL)
-soup = BeautifulSoup(response.text, "html.parser")
+    type_to_ratings = {
+        "Klasika": "gray",
+        "Rapid"  : "red" ,
+        "Blitz"  : "blue"
+    }
 
-for item in soup.findAll('div', attrs={'class':'col-lg-12 profile-top-rating-dataCont'}):
-    for key in ratings:
-        rating = item.find('div', attrs={'class' : f'profile-top-rating-data profile-top-rating-data_{key}'})
-        print(ratings[key], rating.text[6:].strip())
+
+    response = requests.get(URL)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    for item in soup.findAll('div', attrs={'class':'col-lg-12 profile-top-rating-dataCont'}):
+        rating = item.find('div', attrs={'class' : f'profile-top-rating-data profile-top-rating-data_{type_to_ratings[event_type]}'})
+        return rating.text[6:].strip()
+
+if __name__ == "__main__":
+    print("Klasika:", rating("Klasika"))
+    print("Rapid:", rating("Rapid"))
+    print("Blitz:", rating("Blitz"))
