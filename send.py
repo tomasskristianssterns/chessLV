6642dbb07b0be6327d50a01940e8ed8af4b46936
tomasskristianssterns <tomass-kristians.sterns@edu.rtu.lv>
2024-01-05 20:41:08 +0200

    send email from UI

--- send.py
+++ send.py
@@ -1,28 +1,43 @@
 import smtplib 
 import email
 from email.message import EmailMessage
-from user_info import MY_EMAIL, PASSWORD_KEY, MESSAGE
+from user_info import MY_EMAIL, PASSWORD_KEY, FIDE_ID
 
-# SMTP Server and port no for GMAIL.com
-gmail_server= "mail.inbox.lv"
-gmail_port= 587
+def send_email(email, tournament_name, rating):
+    # SMTP Server and port no for inbox.lv
+    inbox_server= "mail.inbox.lv"
+    inbox_port= 587
 
+    # Starting connection
+    my_server = smtplib.SMTP(inbox_server, inbox_port)
+    my_server.ehlo()
+    my_server.starttls()
+        
+    # Login with your email and password
+    my_server.login(MY_EMAIL, PASSWORD_KEY)
+    msg = EmailMessage()
+    msg.set_content(f'''                                     
+    Labdien!
 
-# Starting connection
-my_server = smtplib.SMTP(gmail_server, gmail_port)
-my_server.ehlo()
-my_server.starttls()
-    
-# Login with your email and password
-my_server.login(MY_EMAIL, PASSWORD_KEY)
-msg = EmailMessage()
-msg.set_content(MESSAGE)
+    Nosūtu savu pieteikumu uz {tournament_name}:
 
-msg['Subject'] = 'Testa mail'
-msg['From'] = MY_EMAIL
-msg['To'] = MY_EMAIL
-msg['CC'] = MY_EMAIL
+    Vārds: (vards)
+    Uzvārds: (uzvards)
+    FIDE ID: {FIDE_ID}
+    Reitings: {rating}
+    Sporta klase/tituls: (sporta klase)
+    Pilsēta/klubs: (pilseta)
 
-my_server.send_message(msg)
-print('Mail Sent to', msg['To'], msg['CC'])
-# print(msg)
+    Ar cieņu
+    (vards uzvards)
+
+    Šī ziņa tika sagatavota un nosūtīta automātiski.
+                ''')
+
+    msg['Subject'] = tournament_name
+    msg['From'] = MY_EMAIL
+    msg['To'] = email
+    msg['CC'] = MY_EMAIL
+
+    my_server.send_message(msg)
+    print('Mail Sent to', msg['To'], msg['CC'], msg['Subject'])
