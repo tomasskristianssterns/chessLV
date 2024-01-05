43203093ccc7f95609e71a24db8ab47280c6f6e8
tomasskristianssterns <tomass-kristians.sterns@edu.rtu.lv>
2024-01-05 20:38:51 +0200

    update user_info

--- user_info_example.py
+++ user_info_example.py
@@ -2,13 +2,3 @@ PATH = r"C:\\Users\\{path}\\chessLV\\regulations" # add path to current workspac
 FIDE_ID = ""                                      # change to your fide ID number
 MY_EMAIL = ""                                     # change to your email
 PASSWORD_KEY = ""                                 # change to your generated password key
-MESSAGE = '''                                     
-Labdien!
-
-Šis ir teksts.
-
-Ar cieņu
-Vārds Uzvārds
-
-Šī ziņa tika nosūtīta automātiski.
-                '''                               # change to message
