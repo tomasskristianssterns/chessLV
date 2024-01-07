2905f558660db37fa5163c8e5b53fbd3164cb37b
tomasskristianssterns <tomass-kristians.sterns@edu.rtu.lv>
2024-01-07 18:36:33 +0200

    add EMAIL_SERVER

--- user_info_example.py
+++ user_info_example.py
@@ -2,3 +2,4 @@ PATH = r"C:\\Users\\{path}\\chessLV\\regulations" # add path to current workspac
 FIDE_ID = ""                                      # change to your fide ID number
 MY_EMAIL = ""                                     # change to your email
 PASSWORD_KEY = ""                                 # change to your generated password key
+EMAIL_SERVER = "mail.inbox.lv"                    # change to your email server
